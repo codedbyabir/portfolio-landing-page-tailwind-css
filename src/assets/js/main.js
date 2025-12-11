578c31478b31b901b5f011297c2914f23e20fb95
codedbyabir <abir.supreox@gmail.com>
2025-12-12 01:10:16 +0600

    portfolio & skills section adde

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,4 @@
+// Menu 
 document.addEventListener('DOMContentLoaded', () => {
     // 1. Select the necessary elements from the DOM
     const menuButton = document.getElementById('menu-button'); // The hamburger/toggle icon
@@ -11,4 +12,35 @@ document.addEventListener('DOMContentLoaded', () => {
             mobileMenu.classList.toggle('hidden');
         });
     }
+});
+
+
+
+// Skill Bar 
+document.addEventListener("DOMContentLoaded", () => {
+    // Animate skill bars
+    document.querySelectorAll(".skill-bar").forEach(bar => {
+        const targetWidth = bar.getAttribute("data-width");
+
+        bar.style.width = "0%";
+        setTimeout(() => {
+            bar.style.width = targetWidth;
+        }, 200);
+    });
+
+// Animate numbers
+    document.querySelectorAll(".skill-count").forEach(counter => {
+        let target = parseInt(counter.getAttribute("data-target"));
+        let current = 0;
+        let speed = 20; // Smaller = faster
+
+        let interval = setInterval(() => {
+            if (current < target) {
+                current++;
+                counter.textContent = current + "%";
+            } else {
+                clearInterval(interval);
+            }
+        }, speed);
+    });
 });
